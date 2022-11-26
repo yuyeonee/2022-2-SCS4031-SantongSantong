fbfb34a3bfd0e25d9adbabb7182b969923406c7e
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2022-11-26 15:03:18 +0900

    feat(backend): 소켓 통신 셋팅

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import os
 
 # Create your models here.
+class Drowning(models.Model):
+    id = models.IntegerField
+    pub_date = models.DateTimeField
+    image = models.ImageField(blank=True, null=True)
--- backend/models/views.py
+++ backend/models/views.py
@@ -1,3 +1,13 @@
+from datetime import timezone
+from pathlib import Path
 from django.shortcuts import render
+from backend.models.models import Drowning
 
 # Create your views here.
+def create():
+    file_path = Path("./static/drowning.jpg")
+    if file_path.exists():
+        new_drowning = Drowning()
+        new_drowning.id = 1
+        new_drowning.pub_date = timezone.now()
+        new_drowning.image = "./static/drowning.jpg"
